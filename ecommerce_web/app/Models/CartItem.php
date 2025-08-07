8a045d023f0f1fac947360a7d2e16a5f42e9c2dd
LTKhanhh <letrongkhanh26112004@gmail.com>
2025-08-07 19:15:47 +0700

    [Task BE#90730] define model

--- ecommerce_web/app/Models/CartItem.php
+++ ecommerce_web/app/Models/CartItem.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity'
+    ];
+
+    /**
+     * Relationship with User model
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Relationship with Product model
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
 }
